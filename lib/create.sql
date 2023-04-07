e7adb754b0358e8abec76dc8f7143022abf4cef5
Yael Harosh <yaelharosh01@gmail.com>
2023-04-07 19:15:00 -0400

    did not pass tests but finished lab

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTERGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+)
---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 20, 'M', 'black', 'angry', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 30, 'M', 'blue', 'goofy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 60, 'F', 'yellow', 'slow', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 25, 'M', 'white', 'strict', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 20, 'F', 'brown', 'lazy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 25, 'F', 'tan', 'eh', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 16, 'F', 'red', 'happy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
